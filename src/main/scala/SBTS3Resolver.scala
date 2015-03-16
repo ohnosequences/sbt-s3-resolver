7c5adba4e9a55921f999f2c00aeb34cf35195a92
Alexey Alekhin <laughedelic@gmail.com>
2015-03-16 19:52:02 +0100

    Moved region parameter to converter

--- src/main/scala/SBTS3Resolver.scala
+++ src/main/scala/SBTS3Resolver.scala
@@ -18,13 +18,13 @@ object SbtS3Resolver extends Plugin {
   lazy val showS3Credentials = TaskKey[Unit]("showS3Credentials", "Just outputs credentials that are loaded by the s3credentials provider")
 
   // S3 bucket url
-  case class s3(region: String, url: String) {
+  case class s3(url: String) {
     // adds 's3://' prefix if it was not there
     override def toString: String = "s3://" + url.stripPrefix("s3://")
     
     // convenience method, to use normal bucket addresses with `at`
-    // without this resolver: "foo" at s3(s3region.value, "maven.bucket.com")
-    def toHttps: String = s"""https://s3-${region}.amazonaws.com/${url.stripPrefix("s3://")}"""
+    // without this resolver: "foo" at s3("maven.bucket.com").toHttps(s3region.value)
+    def toHttps(region: String): String = s"""https://s3-${region}.amazonaws.com/${url.stripPrefix("s3://")}"""
   }
 
   case class S3Resolver
